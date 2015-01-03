b82803cc746103754bf65a02eee374222139e88f
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-01-03 12:42:19 +0530

    Delete Numberclosedrectangles.cpp

--- problem/Graph/Numberclosedrectangles.cpp
+++
@@ -1,9 +0,0 @@
-/*
-
-Given a matrix containing only 1 and 0, find the number of closed rectangles. A closed rectangle is represented by at least one 0 surrounded by eight 1′s. 
-There is no overlap of closed rectangles.
-
-
-*/
-
-
